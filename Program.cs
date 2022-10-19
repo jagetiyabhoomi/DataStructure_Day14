eb3ea17fb645271b2a82081b9f4a2ea8b7e5c42c
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-19 23:23:54 +0530

    Usecase 4

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@
             queue.Enqueue(30);
             queue.Enqueue(70);
             Console.WriteLine("element added!!");
+            //Console.WriteLine("Queue Front : {0}",queue.front.key);
+            //Console.WriteLine("Queue Rear : {0}", queue.rear.key);
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            Console.WriteLine("Elements removed!!");
 
 
         }
