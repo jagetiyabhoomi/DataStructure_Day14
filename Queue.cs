b347afd7285ae84f53ea9ef9eb6654361b0546e7
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-19 23:16:43 +0530

    Queue Operations

---
+++ Queue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Baseline.ImTools;
+
+namespace DataStructureDay14
+{
+    public class Queue
+    {
+        public Node front,rear;
+        public Queue()
+        {
+            this.front = this.rear= null;
+        }
+
+        public void enqueue(int key)
+        {
+            Node temp = new Node(key);
+
+            if (this.rear == null)
+            {
+                this.front = this.rear = temp;
+                return;
+            }
+            this.rear.next = temp;
+            this.rear = temp;
+        }
+        public void dequeue()
+        {
+            if (this.front == null)
+                return;
+
+            this.front = this.front.next;
+
+            if (this.front == null)
+            {
+                this.rear = null;
+            }
+        }
+
+    }
+}
