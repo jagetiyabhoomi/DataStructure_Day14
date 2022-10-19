0ba3d5dfb807053e337bbc854484abe5c001b659
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-19 22:53:17 +0530

    UseCase 1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DataStructureDay14
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           Stack<int> stack = new Stack<int>();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            Console.WriteLine("Elements Pushed!!");
+            
+
+
+        }
+    }
+}
